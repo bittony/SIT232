f5a008b845b74493b269e2cf252ea9f6e577f772
bittony <40973917+bittony@users.noreply.github.com>
2021-03-22 22:59:59 +1100

    week2

---
+++ MobileProgram/MobileProgram/Mobile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileProgram
+{
+    class Mobile
+    {
+        private string accType, device, number;
+        private double balance;
+
+        public Mobile(string accType, string device, string number)
+        {
+            this.accType = accType;
+            this.device = device;
+            this.number = number;
+            this.balance = 0.0;
+        }
+        public string getAccType()
+        {
+            return this.accType;
+        }
+        public string getDevice()
+        {
+            return this.device;
+        }
+        public string getNumber()
+        {
+            return this.number;
+        }
+        public string getBalance()
+        {
+            return this.balance.ToString("C");
+        }
+    }
+}
---
+++ MobileProgram/MobileProgram/MobileProgram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MobileProgram
+{
+    class MobileProgram
+    {
+        static void Main(string[] args)
+        {
+            Mobile jimMobile = new Mobile("Monthly", "Samsung Galaxy S6", "07712223344");
+
+            Console.WriteLine("Account Type: " + jimMobile.getAccType() + "\nMobile Number: "
+                + jimMobile.getNumber() + "\nDevice: " + jimMobile.getDevice() + "\nBalance: "
+                + jimMobile.getBalance());
+
+            Console.ReadLine();
+        }
+    }
+}
